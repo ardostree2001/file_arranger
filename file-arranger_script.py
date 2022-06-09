53db03ae4317df70987a132d0a0ab9263a1cbfd3
ardostree2001 <ardostree2001@outlook.com>
2022-06-09 13:16:04 +0530

    Basically Stage 1 is completed
    1. Created a file

---
+++ file-arranger_script.py
@@ -0,0 +1,105 @@
+# Author Name: Harsh Vyapari
+# Author Email: ardostree2001@outlook.com
+# Date of Creation: 09/06/2022
+
+
+# Description : So basically, I designed this script as one day challenge
+# for transferring files for the location given according to the users which automate 
+# the task for file transfer in windows.
+
+
+import os, shutil as files, time as t
+from packages import msg  
+
+msg.test()
+# Four global variables which are going to perform this task
+global src_folder
+global des_folder
+global file_ext
+global file_name
+
+
+
+print("WARNING: Some dialogues in this app are pun intended, so please don't take it seriously (Not that I care!!)")
+t.sleep(5)
+
+# The below function is CLI menu driven function.
+# This function helps in selecting the options.
+# START
+def main_menu():
+
+        try:
+                option = int(input("\n\n******MAIN MENU******: \n 1.Copy Files(According to format) \n 2.Move files(According to format) \n 3.Copy Files \n 4.Move Files \n 5.Reselecting the directory path \n\n \"Enter '0' to Exit\" \n  :$"))
+
+        except ValueError:
+                print("You entered a character/decimal instead of digit, Are you serious!!, \"Please only select number I beg of you.\"")
+                t.sleep(5)
+                return main_menu()
+
+        if option == 1:
+                print("\nFirst Option is Selected:")
+                format_copy()
+                t.sleep(2)
+                return main_menu()
+
+        elif option == 2:
+                print("\nSecond Option is Selected:")
+                format_move() 
+                t.sleep(2)
+                return main_menu()
+        
+        elif option == 3:
+                print("\nThird option is Selected:")
+                direct_copy()
+                t.sleep(2)
+                return main_menu()
+
+        elif option == 4:
+                print("\nFourth option is Selected:")
+                direct_move()
+                t.sleep(2)
+                return main_menu()
+
+        elif option == 5:
+                print("\nFifth option is selected")
+                t.sleep(2)
+                return main_menu()
+
+        elif option == 0:
+                print("\nKono CLI apuri o go riyō itadaki arigatōgozaimasu (English Translation: Thank you for using this CLI app)!!!")
+                t.sleep(3)
+                exit()
+        
+        else:
+                print("Option Selection Error!!, Please select option seriously or I'll beat you to death")
+                return main_menu()
+# END
+
+# All Function Related to Menu Option 
+# (START)
+def format_copy():
+        print("\nFormat copy function")
+        return main_menu()
+
+def format_move():
+        print("\nFormat move function")
+        return main_menu()
+
+def direct_copy():
+        print("\nDirect copy function")
+        return main_menu()
+
+def direct_move():
+        print("\nDirect move function")
+        return main_menu()
+# (END)
+
+# All File name, path are stored here.
+def user_data_collector():
+        print("\nUser data collector")
+        des_folder = input("Destination: ")
+        des_folder = input("Source: ")
+        file_name = input("File Name: ")
+        return main_menu()
+
+user_data_collector()
---
+++ packages/msg.py
@@ -0,0 +1,2 @@
+def test():
+       return print("package is working")
